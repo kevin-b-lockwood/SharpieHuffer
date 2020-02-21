54303e2b7fa5aeac5e3ffe6246150b212d1dc59f
diogenes <kevin-b-lockwood@gmail.com>
2020-02-20 19:05:20 -0800

    Mickey roast complete

--- SharpieHuffer.py
+++ SharpieHuffer.py
@@ -1,17 +1,17 @@
 import discord
 client = discord.Client()
 
-with open("sibling_words.txt", "r") as keywords:
-    sibling_words = keywords.readlines()
+with open("sibling_words.txt", "r") as sibling_keywords:
+    sibling_words = sibling_keywords.readlines()
 
-for i in range(len(keywords)):
-    keywords[i] = keywords[i].strip()
+for i in range(len(sibling_words)):
+    sibling_words[i] = sibling_words[i].strip()
 
 @client.event
 async def on_message(message):
     has_sent = False
-    for x in keywords:
-        if(message.content.lower().count(x.lower()) > 0 and has_sent == False and message.author.name != "SharpieHuffer" and message.author.name == "Mickey")
+    for x in sibling_words:
+        if (message.content.lower().count(x.lower()) > 0 and has_sent == False and message.author.name != "SharpieHuffer" and message.author.name == "Mickey"):
             channel = message.channel
             await channel.send("Did you know: Mickey has too many siblings to count")
             has_sent = True
